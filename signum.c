1fd3b7fa5e33978299400923703183c8cbc0863a
Oliver Zocha <oliverzocha@gmail.com>
2020-06-15 12:12:50 +0200

    Expression.c vollständig signum.c fast fertig

---
+++ signum.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int signum(int a){
+    if ((a < 0))
+    return 1;
+    
+    else if ((a == 0))
+    return 0;
+    
+    else ((a > 0));
+    return -1;
+    
+}
+int main () {
+    int a;
+    printf("Bitte eine ganze Zahl eingeben: ");
+    scanf("%d", &a);
+
+    printf(" das ergebnis der eingabe ist %d\n", signum);
+
+    return 0;
+
+}
+
+
